353a110111301def31967a741d1d267c2d3ec880
Les Lee <les.lee@slotfactory.com>
2019-07-26 15:06:34 +0800

    refactor

---
+++ README.md
@@ -0,0 +1,19 @@
+# HASHMAC
+
+This is a tool for calculate the hashmac of string with SHA256 and base64 encrypt.
+
+Use `--secret` or `-s` to give secret key with string.
+
+## Example
+
+```
+$ hmac-tool '{"a": 1, "b":2}' -s "secret key"
+
+// hmac : z+CQKuPYevATybEqDfiu609r7VP1K1EtDjiAVBRfEDE=
+```
+
+# License
+MIT
+
+## Keywords
+hamc
--- index.js
+++ index.js
@@ -7,22 +7,29 @@ const argv = parseArgs(process.argv.slice(2), {
     string: ['s', 'secret', '_']
 });
 
-// console.log(argv, JSON.parse(argv._[0]));
+if ( !argv._.length && !argv.secret ) {
+    usage();
+    process.exit(0);
+}
 
-const secret = argv.secret;
-const param_str = argv._[0];
+(function () {
+    const crypto = require('crypto');
+    const secret = argv.secret || '';
+    const param_str = argv._[0] || '';
 
+    //creating hmac object
+    const hmac = crypto.createHmac('sha256', secret);
 
-//Name of the file : sha256-hmac.js
-//Loading the crypto module in node.js
-var crypto = require('crypto');
-//creating hmac object
-var hmac = crypto.createHmac('sha256', secret);
-//passing the data to be hashed
-const data = hmac.update(param_str);
-//Creating the hmac in the required format
-const gen_hmac = data.digest('base64');
-//Printing the output on the console
-console.log("secret : " + secret);
-console.log("params : " + param_str);
-console.log("hmac : " + gen_hmac);
+    //passing the data to be hashed
+    const data = hmac.update(param_str);
+
+    //Creating the hmac in the required format
+    const gen_hmac = data.digest('base64');
+
+    console.log(`hmac : ${gen_hmac}`);
+})();
+
+function usage() {
+    console.log()
+}
+
